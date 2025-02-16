bc3cb09a0f1c1cf4932c2908045a54104fce030b
Geoffrey Trenholm <trenholg@sheridancollege.ca>
2025-02-15 21:49:20 -0500

    prototype PasswordRecord and RandomNumberGenerator

--- src/main/java/ca/sheridancollege/trenholg/passwordstoretrenholg/beans/PasswordRecord.java
+++ src/main/java/ca/sheridancollege/trenholg/passwordstoretrenholg/beans/PasswordRecord.java
@@ -1,4 +1,40 @@
 package ca.sheridancollege.trenholg.passwordstoretrenholg.beans;
 
+import ca.sheridancollege.trenholg.passwordstoretrenholg.utilities.RandomNumberGenerator;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import java.util.Random;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PasswordRecord {
+
+    @Id
+    @NonNull
+    private Long ID;
+
+    private String Title;
+    private String Username;
+    private String Password;
+    private String Url;
+    private String Email;
+    private String Notes;
+
+    public PasswordRecord(final String title, String username, final String password, final String url, final String email, final String notes) {
+        this.ID = RandomNumberGenerator.genLongID();
+        this.Title = title;
+        this.Username = username;
+        this.Password = password;
+        this.Url = url;
+        this.Email = email;
+        this.Notes = notes;
+    }
+
 }
--- src/main/java/ca/sheridancollege/trenholg/passwordstoretrenholg/utilities/RandomNumberGenerator.java
+++ src/main/java/ca/sheridancollege/trenholg/passwordstoretrenholg/utilities/RandomNumberGenerator.java
@@ -1,4 +1,10 @@
 package ca.sheridancollege.trenholg.passwordstoretrenholg.utilities;
 
+import java.util.Random;
 public class RandomNumberGenerator {
+    private static final Random rand = new Random();
+
+    public static Long genLongID(){
+        return 100_000_000L + rand.nextLong(999_999_999);
+    }
 }
